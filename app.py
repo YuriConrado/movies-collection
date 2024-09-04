d885fbdc28fc67ebf3cc8f894bd6ed7947872383
YuriConrado <yuriconradosantos123@gmail.com>
2024-09-04 12:28:52 -0300

    aula front-end01

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+API_URL ='https://127.0.0.1:5000/api/movies'
+
 @app.route('/', methods=['GET'])
 def index():
+    response = request.get(API_URL)
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
